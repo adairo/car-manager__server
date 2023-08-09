7b4f5e3a20ee64015074e313aa494a27a5f7b081
adair <adair.reyes@spore.cloud>
2023-08-08 18:37:49 -0600

    implement getCarByPlate function

--- src/components/cars/cars.model.js
+++ src/components/cars/cars.model.js
@@ -1,6 +1,21 @@
 import sequelize from "../../lib/db.js";
 import { DataTypes } from "sequelize";
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Car:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           format: int32
+ *           example: 45
+ *         plate:
+ *           type: string
+ *           example: ATHK455
+ */
 const CarModel = sequelize.define("Car", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   plate: {
